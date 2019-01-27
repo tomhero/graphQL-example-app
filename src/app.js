0ee2e7a02afff0d6e3b23699dcb4cfca72c6e5b0
Chayut <chayut.191@gmail.com>
2019-01-28 00:00:14 +0700

    Create mongo db connection

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ const graphqlHttp = require('express-graphql')
 const {
   buildSchema
 } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
 
-const events = []
-
 app.use(bodyParser.json())
-
-app.listen(3000)
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
@@ -47,22 +46,50 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events
+      return Event.find()
+        .then(events => {
+          return events.map(event => {
+            return {
+              ...event._doc,
+              _id: event.id
+            }
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          throw err
+        })
     },
-    createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+    createEvent: args => {
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: args.eventInput.price,
-        date: new Date().toString(),
+        date: new Date(args.eventInput.date),
         comment: args.eventInput.comment
-      }
-      events.push(event)
+      })
+      event
+        .save()
+        .then(rs => {
+          console.log(rs)
+          return { ...rs._doc, _id: rs._doc._id.toString()
+          }
+        }).catch(err => {
+          console.log(err)
+          throw err
+        })
     }
   },
   graphiql: true
 }))
+
+// To access databse -> click https://cloud.mongodb.com
+mongoose.connect(`mongodb+srv://${
+  process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD}@cluster0-nkwtt.mongodb.net/${
+  process.env.MONGO_DB}?retryWrites=tr0ue`)
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res, next) => {
   res.send('Hello GraphQL')
